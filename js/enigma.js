5ab5bd93d9a90417a6c0fbf83c7ddcd431f18220
Andrew Miller <rhuk@mac.com>
2016-12-29 17:15:02 -0700

    Initial commit

---
+++ js/enigma.js
@@ -0,0 +1,40 @@
+var isTouch = window.DocumentTouch && document instanceof DocumentTouch;
+
+function scrollHeader() {
+    // Has scrolled class on header
+    var zvalue = $(document).scrollTop();
+    if ( zvalue > 75 )
+        $("#header").addClass("scrolled");
+    else
+        $("#header").removeClass("scrolled");
+}
+
+jQuery(document).ready(function($){
+
+    // call every load
+    scrollHeader();
+
+    // ON SCROLL EVENTS
+    if (!isTouch){
+        $(document).scroll(function() {
+            scrollHeader();
+        });
+    };
+
+    // TOUCH SCROLL
+    $(document).on({
+        'touchmove': function(e) {
+            scrollHeader(); // Replace this with your code.
+        }
+    });
+
+    //Smooth scroll to top
+    $('#to-top').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+    // Responsive Menu
+
+});
+
+
