5c083e35795865bc17901c0006b0c6c0cabd975d
Andy Miller <rhuk@mac.com>
2017-01-19 09:40:30 -0700

    Added support for `inherit_from` to inherit settings from other pages

--- enigma.php
+++ enigma.php
@@ -2,6 +2,7 @@
 namespace Grav\Theme;
 
 use Grav\Common\Theme;
+use Grav\Common\Utils;
 
 class Enigma extends Theme
 {
@@ -29,7 +30,17 @@ class Enigma extends Theme
 
         // If a page exists merge the configs
         if ($page) {
-            $this->config->set('theme', $this->mergeConfig($page));
+            if (isset($page->header()->enigma['inherit_from'])) {
+                $inherit_from = $page->header()->enigma['inherit_from'];
+                $inherit_page = $this->grav['pages']->dispatch($inherit_from);
+                $current_header = $page->header()->enigma;
+                $inherited_header = $this->mergeConfig($inherit_page)->toArray();
+                $page->modifyHeader($this->name, Utils::arrayMergeRecursiveUnique($inherited_header, $current_header));
+                $this->config->set('theme', $page->header()->enigma);
+            } else {
+                $this->config->set('theme', $this->mergeConfig($page));
+            }
+
         }
     }
 }
